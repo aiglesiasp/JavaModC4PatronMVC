7a32fb8a1a78fb7239942a7dbd1bd482178ecd81
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-11 17:46:57 +0200

    First Commit Project Created

---
+++ JavaModC4PatronMVC/src/main/java/com/aiglesiasp/mvc/JavaModC4PatronMVC/App.java
@@ -0,0 +1,13 @@
+package com.aiglesiasp.mvc.JavaModC4PatronMVC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JavaModC4PatronMVC/src/test/java/com/aiglesiasp/mvc/JavaModC4PatronMVC/AppTest.java
@@ -0,0 +1,20 @@
+package com.aiglesiasp.mvc.JavaModC4PatronMVC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
